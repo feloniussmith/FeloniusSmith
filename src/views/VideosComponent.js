9be41aab04b2ae3e544d691a6d503bc82be3e399
Cameron1044 <cameronpelkey.808@gmail.com>
2023-08-30 10:01:27 -0600

    unused styles

--- src/views/VideosComponent.js
+++ src/views/VideosComponent.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from '../Styles/Pages.module.scss'
+// import styles from '../Styles/Pages.module.scss'
 
 const CalendarComponent = ({ fields }) => {
   const { title } = fields
