f3c30c66682aea64717897ff26dc56abf5b2440d
Cameron1044 <cameronpelkey.808@gmail.com>
2023-09-04 14:50:44 -0600

    added reviews

--- src/views/MusicComponent.js
+++ src/views/MusicComponent.js
@@ -1,22 +1,37 @@
 import React from 'react'
 import styles from '../Styles/Pages.module.scss'
+import ReactMarkdown from 'react-markdown';
 
-const CalendarComponent = ({ fields, musicList = [] }) => {
+const MusicComponent = ({ fields, musicList = [] }) => {
   // const { title, description } = fields
-
   const MusicList = (musicList) => {
     return (
       musicList.map((music, index) => {
         return (
-          <div className={styles.music}>
+          <div className={styles.music} key={index}>
             <h1 className={styles.title}>{music.title}</h1>
             <iframe className={styles.media} allow="autoplay *; encrypted-media *;" frameborder="0" height="450" title="music" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src={music.embed}></iframe>
-            <p className={styles.text}>{music.description}</p>
+            <div className={styles.text}>
+              <ReactMarkdown>
+                {music.description}
+              </ReactMarkdown>
+            </div>
+            <div className={styles.reviews}>
+              {music.reviews && music.reviews.map((review, idx) => (
+                <div key={idx}>
+                  <blockquote className={styles.reviewText}>
+                    <ReactMarkdown>{review.text}</ReactMarkdown>
+                  </blockquote>
+                  <cite className={styles.reviewAuthor}>— {review.author}</cite>
+                </div>
+              ))}
+            </div>
           </div>
         );
       })
     )
   }
+
   return (
     <>
       {MusicList(musicList)}
@@ -24,4 +39,4 @@ const CalendarComponent = ({ fields, musicList = [] }) => {
   )
 }
 
-export default CalendarComponent
+export default MusicComponent
