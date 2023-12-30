157a7f9588c2ddd94a1d16cd9026b5539b45e389
mamapagetee <ethan0jago@gmail.com>
2023-12-29 23:51:11 -0800

    asd

--- src/views/HomeComponent.js
+++ src/views/HomeComponent.js
@@ -34,7 +34,6 @@ const HomeComponent = ({ fields }) => {
       <iframe className={styles.media} allow="autoplay *; encrypted-media *;" frameborder="0" height="450" title="music" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src={embed}></iframe>
 
     </div>
-    <div style={{height: 2000}}></div>
     </>
   );
 }
