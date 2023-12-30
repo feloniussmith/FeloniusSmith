6ccd0b4c9a5e80f5969a31f886a9a1e7df661320
mamapagetee <ethan0jago@gmail.com>
2023-12-29 21:29:17 -0800

    Got rid of bubble

--- src/views/NavGuitar.js
+++ src/views/NavGuitar.js
@@ -13,11 +13,11 @@ const NavGuitar = () => {
   return (
     <>
       <NavLink to='/videos' className={styles.dotButton} style={{top: '93px', left: '2.806122448979592%'}} activeClassName='active'><img src={navVideos} alt='felonius-videos' /></NavLink>
-      <NavLink to='/calendar' className={styles.dotButton} style={{top: '99.5px', left: '8.3%'}} activeClassName='active'><img src={navCalendar} alt='felonius-calendar' /></NavLink>
-      <NavLink to='/music' className={styles.dotButton} style={{top: '165px', left: '9.33673469387755%'}} activeClassName='active'><img src={navMusic} alt='felonius-music' /></NavLink>
+      <NavLink to='/calendar' className={styles.dotButton} style={{top: '99px', left: '8.3%'}} activeClassName='active'><img src={navCalendar} alt='felonius-calendar' /></NavLink>
+      <NavLink to='/music' className={styles.dotButton} style={{top: '165px', left: '9.4%'}} activeClassName='active'><img src={navMusic} alt='felonius-music' /></NavLink>
       <NavLink to='/presskit' className={styles.dotButton} style={{top: '226px', left: '5%'}} activeClassName='active'><img src={navPresskit} alt='felonius-presskit' /></NavLink>
       <NavLink to='/contact' className={styles.dotButton} style={{top: '241.4px', left: '11.6%'}} activeClassName='active'><img src={navContactPhotos} alt='felonius-contact' /></NavLink>
-      <NavLink to='/' className={styles.dotButton} style={{top: '725px', left: '18.5%'}} activeClassName='active'><img src={navHome} alt='felonius-home' /></NavLink>
+      <NavLink to='/' className={styles.dotButton} style={{top: '725px', left: '18%'}} activeClassName='active'><img src={navHome} alt='felonius-home' /></NavLink>
     </>
   )
 }
