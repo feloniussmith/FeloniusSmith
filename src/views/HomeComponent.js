bb970e2e21533a9aa1e69378dfbb0b0b502dc5c3
mamapagetee <ethan0jago@gmail.com>
2023-12-29 23:33:30 -0800

    Bigger Home

--- src/views/HomeComponent.js
+++ src/views/HomeComponent.js
@@ -34,7 +34,7 @@ const HomeComponent = ({ fields }) => {
       <iframe className={styles.media} allow="autoplay *; encrypted-media *;" frameborder="0" height="450" title="music" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src={embed}></iframe>
 
     </div>
-    <div style={{height: 700}}></div>
+    <div style={{height: 750}}></div>
     </>
   );
 }
