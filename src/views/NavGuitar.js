77ee3d0b11157a2d35dc3d279fa44f22ae005b94
mamapagetee <ethan0jago@gmail.com>
2023-12-29 21:26:08 -0800

    Nav guitar

--- src/views/NavGuitar.js
+++ src/views/NavGuitar.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React from 'react';
 import { NavLink } from 'react-router-dom';
-import styles from '../Styles/Nav.module.scss'
+import styles from '../Styles/Nav.module.scss';
+
+import navHome from '../Images/feloniushome.png';
+import navVideos from '../Images/feloniusVideos.png';
+import navPresskit from '../Images/feloniusPresskit.png';
+import navContactPhotos from '../Images/feloniusContact.png';
+import navMusic from '../Images/feloniusMusic.png';
+import navCalendar from '../Images/feloniusCalendar.png';
 
 const NavGuitar = () => {
   return (
     <>
-      <NavLink to='/videos' className={styles.dotButton} style={{top: '91px', left: '2.806122448979592%'}} activeClassName='active'></NavLink>
-      <NavLink to='/calendar' className={styles.dotButton} style={{top: '76px', left: '13.316326530612244%'}} activeClassName='active'></NavLink>
-      <NavLink to='/music' className={styles.dotButton} style={{top: '158px', left: '9.33673469387755%'}} activeClassName='active'></NavLink>
-      <NavLink to='/presskit' className={styles.dotButton} style={{top: '229px', left: '4.948979591836735%'}} activeClassName='active'></NavLink>
-      <NavLink to='/contact' className={styles.dotButton} style={{top: '211px', left: '15.96938775510204%'}} activeClassName='active'></NavLink>
-      <NavLink to='/' className={styles.dotButton} style={{top: '720px', left: '19.5%'}} activeClassName='active'></NavLink>
+      <NavLink to='/videos' className={styles.dotButton} style={{top: '93px', left: '2.806122448979592%'}} activeClassName='active'><img src={navVideos} alt='felonius-videos' /></NavLink>
+      <NavLink to='/calendar' className={styles.dotButton} style={{top: '99.5px', left: '8.3%'}} activeClassName='active'><img src={navCalendar} alt='felonius-calendar' /></NavLink>
+      <NavLink to='/music' className={styles.dotButton} style={{top: '165px', left: '9.33673469387755%'}} activeClassName='active'><img src={navMusic} alt='felonius-music' /></NavLink>
+      <NavLink to='/presskit' className={styles.dotButton} style={{top: '226px', left: '5%'}} activeClassName='active'><img src={navPresskit} alt='felonius-presskit' /></NavLink>
+      <NavLink to='/contact' className={styles.dotButton} style={{top: '241.4px', left: '11.6%'}} activeClassName='active'><img src={navContactPhotos} alt='felonius-contact' /></NavLink>
+      <NavLink to='/' className={styles.dotButton} style={{top: '725px', left: '18.5%'}} activeClassName='active'><img src={navHome} alt='felonius-home' /></NavLink>
     </>
   )
 }
