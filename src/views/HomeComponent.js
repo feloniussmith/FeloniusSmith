ad0ef2977ff2d6fd81116b2756d69134ecd8aa00
Cameron1044 <cameronpelkey.808@gmail.com>
2024-01-26 15:47:16 -0700

    fixed home text

--- src/views/HomeComponent.js
+++ src/views/HomeComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from '../Styles/Home.module.scss';
-import ReactMarkdown from 'react-markdown';
+// import ReactMarkdown from 'react-markdown';
 import IMG from '../Images/jeffSmith1.png';
 
 const HomeComponent = ({ fields }) => {
@@ -9,34 +9,26 @@ const HomeComponent = ({ fields }) => {
   return (
     <>
     <div className={styles.mainText}>
-      <h1><ReactMarkdown>{text1}</ReactMarkdown></h1>
+      <h1>{text1}</h1>
     </div>
     <div className={styles.homeContainer}>
 
       <div className={styles.review}>
         <img className={styles.mainImage} src={IMG} alt="Jeff Smith" />
         <div className={styles.reviewText}>
-          <ReactMarkdown>
             {text}
-          </ReactMarkdown>
         </div>
         <div className={styles.reviewAuthor}>
-          <ReactMarkdown>
             {author}
-          </ReactMarkdown>
         </div>
       </div>
 
       <div className={styles.musicInfo}>
         <div className={styles.musicText}>
-          <ReactMarkdown>
             {musictext}
-          </ReactMarkdown>
         </div>
         <div className={styles.musicSubtext}>
-          <ReactMarkdown>
             {musicsubtext}
-          </ReactMarkdown>
         </div>
       </div>
 
