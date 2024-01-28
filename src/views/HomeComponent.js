ed567b97f41b1872117d8c8d74338607cf8f05e5
Cameron1044 <cameronpelkey.808@gmail.com>
2024-01-28 04:20:09 -0700

    centered media player on home

--- src/views/HomeComponent.js
+++ src/views/HomeComponent.js
@@ -31,7 +31,9 @@ const HomeComponent = ({ fields }) => {
             {musicsubtext}
         </div>
       </div>
-      <Player name={name} cover={albumcover} songs={songs} />
+      <div className={styles.player}>
+        <Player name={name} cover={albumcover} songs={songs} />
+      </div>
       {/* <iframe className={styles.media} allow="autoplay *; encrypted-media *;" frameborder="0" height="450" title="music" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src={embed}></iframe> */}
     </div>
     </>
